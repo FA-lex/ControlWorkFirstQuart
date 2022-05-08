0d52560666610ffc8435bb037bf811d3b755c111
FA-lex <a.good.lex@gmail.com>
2022-05-08 18:52:08 +0800

    create function find three char long elements from massiv

---
+++ program/Program.cs
@@ -0,0 +1,35 @@
+﻿
+string[] findMaxThreeChar(string[] array)
+{
+    string s = string.Empty;
+    int count = 0;
+    for(int i =0; i < array.Length; i++)
+    {
+        if(array[i].Length < 4)
+        {
+            if(count == 0)
+            {
+                s = array[i];
+                count++;
+            }
+            else s = $"{s}, {array[i]}";  
+        }
+    }
+    string[] rezult = s.Split(",")
+            .Select(e=>(e))
+            .ToArray();
+    return rezult;
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
+}
+
+string[] inputDate = new string[]{"j", "ndh", "kfi)", "dhyrj", "jjj"};
+PrintArray(inputDate);
+string[] array;
+array = findMaxThreeChar(inputDate);
+PrintArray(array);
